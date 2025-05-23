c740adfd7885f084b4b310aae1b146edfdc92c81
Rory Doak <rory.doak@gmail.com>
2025-05-23 15:03:22 +0100

    wire up new RequestError type

--- sh24-client/src/components/ErrorBox/ErrorBox.tsx
+++ sh24-client/src/components/ErrorBox/ErrorBox.tsx
@@ -1,9 +1,10 @@
+import type { RequestError } from "../../types/requests";
 import "./ErrorBox.css";
 
 export default function ErrorBox({
   errorMessage,
 }: {
-  errorMessage: string | null;
+  errorMessage: RequestError | null;
 }) {
-  return <p>{errorMessage}</p>;
+  return <p>{errorMessage?.error.message}</p>;
 }
