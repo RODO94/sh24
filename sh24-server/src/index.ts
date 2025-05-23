3afcd75479568f80415015ce5d677521cc5d2449
Rory Doak <rory.doak@gmail.com>
2025-05-23 15:51:48 +0100

    initial run at checking postcode at API endpoint and validating input

--- sh24-server/src/index.ts
+++ sh24-server/src/index.ts
@@ -1,18 +1,41 @@
 import { config } from "dotenv";
 import express from "express";
 import cors from "cors";
+import { z } from "zod/v4";
 
 // Load environment variables
 config();
 
 const PORT = process.env.PORT || 8080;
 const NODE_ENV = process.env.NODE_ENV || "development";
+const postcodeIoUrl = "https://api.postcodes.io/postcodes";
 
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
+app.get("/postcode/:postcode", async (req, res) => {
+  const { postcode } = req.params;
+  const validatedPostcode = z.string().min(5).max(7).safeParse(postcode);
+
+  if (!validatedPostcode.success) {
+    res.status(400).send({
+      error: `'${postcode}' is in an invalid format. Enter a postcode similar to SW12 4AB`,
+    });
+  }
+
+  try {
+    const response = await fetch(`${postcodeIoUrl}/${postcode}`);
+    if (!response.ok) {
+      res.status(404).json({ error: "Postcode not found" });
+    }
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error fetching postcode data:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 app.get("/health", (_req, res) => {
   res.status(200).json({
