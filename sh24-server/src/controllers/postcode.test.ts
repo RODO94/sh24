814a779d96671b93dd25a8986ecc3307476d10f8
Rory Doak <rory.doak@gmail.com>
2025-05-24 11:28:21 +0100

    add unit tests for allowed service areas

--- sh24-server/src/controllers/postcode.test.ts
+++ sh24-server/src/controllers/postcode.test.ts
@@ -2,13 +2,16 @@ import test, { describe, it } from "node:test";
 import assert from "node:assert";
 import {
   checkAndValidatePostcode,
+  checkIfAllowedServiceArea,
   checkIfPostcodeIsAllowed,
 } from "./utils/validation.js";
 import { southwarkPostcode } from "../__tests__/mocks/southwark.js";
 import { firstSHPostcode } from "../__tests__/mocks/sh24.js";
+import { lambethPostcode } from "../__tests__/mocks/lambeth.js";
 
 const invalidZodPostcode = "INV12 5BGO";
 const invalidPostcode = "xx122nn";
+const invalidServiceArea = "Cambuslang 000";
 
 describe("zod postcode validation", () => {
   it("creates a zod error for invalid postcodes", () => {
@@ -39,6 +42,27 @@ describe("check for allowed postcodes", () => {
   });
 });
 
+describe("check for allowed service areas", () => {
+  it("returns true for allowed service areas", () => {
+    const isAllowedServiceArea = checkIfAllowedServiceArea(
+      "Lambeth 000",
+      lambethPostcode
+    );
+    assert.strictEqual(isAllowedServiceArea, true);
+  });
+
+  it("returns an error for not allowed service areas", () => {
+    const isAllowedServiceArea = checkIfAllowedServiceArea(
+      invalidServiceArea,
+      invalidPostcode
+    );
+    assert.strictEqual(isAllowedServiceArea.error.type, "input");
+    assert.strictEqual(
+      isAllowedServiceArea.error.message,
+      `'${invalidPostcode}' is in Cambuslang which is not an allowed service area. Enter another postcode`
+    );
+  });
+});
 test.todo("the error when a postcode is not found");
 
 test.todo("the postcode is in the service area check");
