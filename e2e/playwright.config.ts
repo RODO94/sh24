2e107e7c67b0fa85c16018f9a4ec57b3e09a35f5
Rory Doak <rory.doak@gmail.com>
2025-05-24 17:42:35 +0100

    playwright e2e config

---
+++ e2e/playwright.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+  use: {
+    baseURL: "http://localhost:3000",
+    trace: "on-first-retry",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+
+  webServer: [
+    {
+      command: "cd sh24-server && npm start",
+      port: 8080,
+      reuseExistingServer: !process.env.CI,
+    },
+    {
+      command: "cd sh24-client && npm run dev",
+      port: 5173,
+      reuseExistingServer: !process.env.CI,
+    },
+  ],
+});
